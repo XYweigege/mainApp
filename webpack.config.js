29e6a19efff0686dfdcb3b0c56fbd366e1e3c3c4
weigedediannao <1753552156@qq.com>
2025-10-19 18:20:40 +0800

    feat: update

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,64 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack').container.ModuleFederationPlugin;
-const path = require('path');
-const deps = require('./package.json').dependencies;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin =
+  require("webpack").container.ModuleFederationPlugin;
+const path = require("path");
+const deps = require("./package.json").dependencies;
 
 module.exports = {
-  entry: './src/index',
-  mode: 'development',
-  target: 'web',
+  entry: "./src/index",
+  mode: "development",
+  target: "web",
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, "dist"),
     },
     port: 3001,
   },
   output: {
-
+    publicPath: "auto",
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/,
         options: {
-          presets: ['@babel/preset-react'],
+          presets: ["@babel/preset-react"],
         },
       },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'mainApp',
+      name: "mainApp",
       // adds react as shared module
       // version is inferred from package.json
       // there is no version check for the required version
       // so it will always use the higher version found
       remotes: {
-        remoteApp: 'remoteApp@https://xyweigege.github.io/component-app/remoteEntry.js',
+        remoteApp:
+          "remoteApp@https://xyweigege.github.io/component-app/remoteEntry.js",
       },
       shared: {
         react: {
-          import: 'react', // the "react" package will be used a provided and fallback module
-          shareKey: 'react', // under this name the shared module will be placed in the share scope
-          shareScope: 'default', // share scope with this name will be used
+          import: "react", // the "react" package will be used a provided and fallback module
+          shareKey: "react", // under this name the shared module will be placed in the share scope
+          shareScope: "default", // share scope with this name will be used
           singleton: true, // only a single version of the shared module is allowed
         },
-        'react-dom': {
-          requiredVersion: deps['react-dom'],
+        "react-dom": {
+          requiredVersion: deps["react-dom"],
           singleton: true, // only a single version of the shared module is allowed
         },
       },
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
   ],
 };
